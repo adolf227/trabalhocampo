0abeb3005522d3b55c15481b216197c178fc6b2a
adolf227 <162127882+adolf227@users.noreply.github.com>
2025-05-29 09:52:57 -0300

    Add files via upload

---
+++ script.cs
@@ -0,0 +1,9 @@
+// Função para mostrar detalhes de comparação
+function mostrarComparacao() {
+    var detalhes = document.getElementById('comparacaoDetalhada');
+    if (detalhes.style.display === "none") {
+        detalhes.style.display = "block";
+    } else {
+        detalhes.style.display = "none";
+    }
+}
